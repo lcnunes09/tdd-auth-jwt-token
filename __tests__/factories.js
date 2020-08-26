b20e799579fd65b3c5459e79b5f2a5a64ad3087e
Luciana Nunes <lcnunes09@gmail.com>
2020-08-25 22:43:29 -0300

    Practicing TDD using Jest and NodeJS - Authentication with JWT Token

---
+++ __tests__/factories.js
@@ -0,0 +1,13 @@
+const faker = require('faker');
+
+const { factory } = require('factory-girl');
+const { User } = require('../src/app/models');
+
+factory.define('User', User, {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+});
+
+module.exports = factory;
+
---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+const { types } = require('pg');
+
+require('dotenv').config({
+  path: process.env.NODE_ENV.trim() === 'test' ? '.env.test' : '.env'
+});
+
+module.exports = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || 'postgres',
+  storage: './__tests__/database.sqlite',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  },
+};
